75ddc1724a2ca11237dcc2079472c80d308b060e
Michael AS <wsy07280012@126.com>
2013-03-11 19:33:26 +0800

    8

--- WinValidateCodeRecognition/MainForm.cs
+++ WinValidateCodeRecognition/MainForm.cs
@@ -75,7 +75,6 @@ namespace WinValidateCodeRecognition
             //}
             #endregion
 
-
             image.Save(@"F:\validateCodeImg\" + imgName + @"_二值化图.jpg");
             image.Dispose();
             g.Dispose();
@@ -92,7 +91,7 @@ namespace WinValidateCodeRecognition
             Bitmap bmpTg = new Bitmap(imgTg);
             Bitmap bmpMT = new Bitmap(imgMT);
 
-            Point[] p = GetTargetPoints(bmpTg, bmpMT, 50);
+            Point[] p = ValidateCodeHelper.GetTargetPoints(bmpTg, bmpMT, 50);
             Image imgTmp = (Image)imgTg.Clone();
             Graphics g = Graphics.FromImage(imgTmp);
             Pen pen = new Pen(Color.Red, 1);
@@ -109,97 +108,8 @@ namespace WinValidateCodeRecognition
             MessageBox.Show("OK");
         }
 
-        #region 获取主体矩形边界坐标
-        /// <summary>
-        /// 获取主体矩形边界坐标，顺时针
-        /// </summary>
-        /// <param name="bmpTg">目标图片</param>
-        /// <param name="bmpMT">模板图片</param>
-        /// <param name="redress">容错率(0-100)</param>
-        /// <returns></returns>
-        private static Point[] GetTargetPoints(Bitmap bmpTg, Bitmap bmpMT, int redress)
-        {
-            //主体矩形边界坐标,顺时针
-            Point[] points ={
-                 new Point(0,0),
-                 new Point(0,0),
-                 new Point(0,0),
-                 new Point(0,0),
-                            };
-
-            int mtMainPxSum = 0;//模板主体像素数
-            int matchPxNum = 0;//目标图片与模板主体匹配成功的像素数
-
-            int tmpRGB = 0;
-            int mtxDiff = 0;//模板图片主体x起始坐标距离0的距离
-            int mtyDiff = 0;//模板图片主体y起始坐标距离0的距离
-            int mtMainW = 0;//模板图片主体宽
-            int mtMainH = 0;//模板图片主体高
-            List<int> mtxs = new List<int>();//模板图片x坐标集合
-            List<int> mtys = new List<int>();//模板图片y坐标集合
-
-            for (int x = 0; x < bmpMT.Width; x++)
-            {
-                for (int y = 0; y < bmpMT.Height; y++)
-                {
-                    if (bmpMT.GetPixel(x, y).B < 150)
-                    {
-                        mtxs.Add(x);
-                        mtys.Add(y);
-                    }
-                }
-            }
-
-            mtxDiff = mtxs.Min() - 0;
-            mtyDiff = mtys.Min() - 0;
-            mtMainW = mtxs.Max() - mtxs.Min() + 1;
-            mtMainH = mtys.Max() - mtys.Min() + 1;
-
-            for (int tgx = 0; tgx < bmpTg.Width - mtMainW; tgx++)
-            {
-                for (int tgy = 0; tgy < bmpTg.Height - mtMainH; tgy++)
-                {
-                    mtMainPxSum = 0;
-                    matchPxNum = 0;
-                    for (int mtx = 0; mtx < mtMainW; mtx++)
-                    {
-                        for (int mty = 0; mty < mtMainH; mty++)
-                        {
-                            if (bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B > 100)
-                                continue;
-                            mtMainPxSum++;//统计模板主体像素数
-                            tmpRGB = bmpTg.GetPixel(tgx + mtx, tgy + mty).B - bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B;
-                            tmpRGB = tmpRGB > 0 ? tmpRGB : tmpRGB * (-1);
-                            if (tmpRGB < 150)//如果匹配
-                            {
-                                matchPxNum++;//统计匹配的像素数
-                            }
-                        }
-                    }
-                    //模板匹配结束一轮后
-                    int accuracy = (int)((matchPxNum * 1.0f / mtMainPxSum) * 100);//正确率
-                    if (accuracy + redress < 100)//如果正确率加上容错率没有大于百分百，说明没有匹配上，进行下一轮匹配
-                    {
-                        continue;
-                    }
-                    points[0].X = tgx;
-                    points[0].Y = tgy;
-                    points[1].X = tgx + mtMainW;
-                    points[1].Y = tgy;
-                    points[2].X = tgx + mtMainW;
-                    points[2].Y = tgy + mtMainH;
-                    points[3].X = tgx;
-                    points[3].Y = tgy + mtMainH;
-                    return points;
-                }
-            }
-            return points;
-        }
-        #endregion
-
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-
 
         }
 
@@ -211,22 +121,31 @@ namespace WinValidateCodeRecognition
         private void button3_Click(object sender, EventArgs e)
         {
 
-            Image myImage = new Bitmap(60, 20);
             HtmlDocument doc = webBrowser1.Document;
 
             doc.GetElementById("startStation_ticketPrice1").Children[1].Children[0].SetAttribute("value", "上海");
             doc.GetElementById("arriveStation_ticketPrice1").Children[1].Children[0].SetAttribute("value", "北京");
 
+
             #region 获取验证码图片
-            IHTMLControlElement img = (IHTMLControlElement)doc.Images["img_rrand_code"].DomElement;
-            IHTMLDocument2 tmpDoc = webBrowser1.Document.DomDocument as IHTMLDocument2;
-            HTMLBody body = (HTMLBody)tmpDoc.body;
-            IHTMLControlRange rang = (IHTMLControlRange)body.createControlRange();
-            rang.add(img);
-            rang.execCommand("Copy", false, null);
-            myImage = Clipboard.GetImage();
-            pictureBox1.Image = myImage;
+            Image valiCodeImg = GetValidateCode("img_rrand_code");
+            string path = @"D:\My Documents\GitHub\WinValidateCodeRecognition\WinValidateCodeRecognition\TemplatePic\";
+            for (int i = 0; i < 10; i++)
+            {
+                DirectoryInfo dir = new DirectoryInfo(path + i);
+                for (int n = 0; n < dir.GetFiles().Count(); n++)
+                {
+                    Image imgTg = Image.FromFile(path + i + n + ".jpg");
+                    Bitmap bmpMT = new Bitmap(imgTg);
+                }
+            }
+            //ValidateCodeHelper.MatchFirstNum(new Bitmap(valiCodeImg),
+
+            pictureBox1.Image = valiCodeImg;
             #endregion
+
+            doc.GetElementById("randCode").SetAttribute("value", "3");
+
             doc.InvokeScript("cccxsubmit", new object[] { "stationDIV", "stationDIV2" });
             StreamWriter sw = new StreamWriter(@"F:\1.txt");
             sw.Write(doc.GetElementById("gridbox").InnerHtml);
@@ -242,20 +161,30 @@ namespace WinValidateCodeRecognition
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+
+            #region 获取验证码图片
+            pictureBox1.Image = GetValidateCode("img_rrand_code");
+            #endregion
+
+        }
+
+        /// <summary>
+        /// 获取验证码图片
+        /// </summary>
+        /// <param name="imgId"></param>
+        /// <returns></returns>
+        private Image GetValidateCode(string imgId)
+        {
             Image myImage = new Bitmap(60, 20);
             HtmlDocument doc = webBrowser1.Document;
-            #region 获取验证码图片
-            IHTMLControlElement img = (IHTMLControlElement)doc.Images["img_rrand_code"].DomElement;
+            IHTMLControlElement img = (IHTMLControlElement)doc.Images[imgId].DomElement;
             IHTMLDocument2 tmpDoc = webBrowser1.Document.DomDocument as IHTMLDocument2;
             HTMLBody body = (HTMLBody)tmpDoc.body;
             IHTMLControlRange rang = (IHTMLControlRange)body.createControlRange();
             rang.add(img);
             rang.execCommand("Copy", false, null);
-            pictureBox1.Image = null;
             myImage = Clipboard.GetImage();
-            pictureBox1.Image = myImage;
-            #endregion
-
+            return myImage;
         }
 
     }
---
+++ WinValidateCodeRecognition/ValidateCodeHelper.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinValidateCodeRecognition
+{
+    public static class ValidateCodeHelper
+    {
+        #region 获取主体矩形边界坐标
+        /// <summary>
+        /// 获取主体矩形边界坐标，顺时针
+        /// </summary>
+        /// <param name="bmpTg">目标图片</param>
+        /// <param name="bmpMT">模板图片</param>
+        /// <param name="redress">容错率(0-100)</param>
+        /// <returns></returns>
+        public static Point[] GetTargetPoints(Bitmap bmpTg, Bitmap bmpMT, int redress)
+        {
+            //主体矩形边界坐标,顺时针
+            Point[] points ={
+                 new Point(0,0),
+                 new Point(0,0),
+                 new Point(0,0),
+                 new Point(0,0),
+                            };
+
+            int mtMainPxSum = 0;//模板主体像素数
+            int matchPxNum = 0;//目标图片与模板主体匹配成功的像素数
+
+            int tmpRGB = 0;
+            int mtxDiff = 0;//模板图片主体x起始坐标距离0的距离
+            int mtyDiff = 0;//模板图片主体y起始坐标距离0的距离
+            int mtMainW = 0;//模板图片主体宽
+            int mtMainH = 0;//模板图片主体高
+            List<int> mtxs = new List<int>();//模板图片x坐标集合
+            List<int> mtys = new List<int>();//模板图片y坐标集合
+
+            for (int x = 0; x < bmpMT.Width; x++)
+            {
+                for (int y = 0; y < bmpMT.Height; y++)
+                {
+                    if (bmpMT.GetPixel(x, y).B < 150)
+                    {
+                        mtxs.Add(x);
+                        mtys.Add(y);
+                    }
+                }
+            }
+
+            mtxDiff = mtxs.Min() - 0;
+            mtyDiff = mtys.Min() - 0;
+            mtMainW = mtxs.Max() - mtxs.Min() + 1;
+            mtMainH = mtys.Max() - mtys.Min() + 1;
+
+            for (int tgx = 0; tgx < bmpTg.Width - mtMainW; tgx++)
+            {
+                for (int tgy = 0; tgy < bmpTg.Height - mtMainH; tgy++)
+                {
+                    mtMainPxSum = 0;
+                    matchPxNum = 0;
+                    for (int mtx = 0; mtx < mtMainW; mtx++)
+                    {
+                        for (int mty = 0; mty < mtMainH; mty++)
+                        {
+                            if (bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B > 100)
+                                continue;
+                            mtMainPxSum++;//统计模板主体像素数
+                            tmpRGB = bmpTg.GetPixel(tgx + mtx, tgy + mty).B - bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B;
+                            tmpRGB = tmpRGB > 0 ? tmpRGB : tmpRGB * (-1);
+                            if (tmpRGB < 150)//如果匹配
+                            {
+                                matchPxNum++;//统计匹配的像素数
+                            }
+                        }
+                    }
+                    //模板匹配结束一轮后
+                    int accuracy = (int)((matchPxNum * 1.0f / mtMainPxSum) * 100);//正确率
+                    if (accuracy + redress < 100)//如果正确率加上容错率没有大于百分百，说明没有匹配上，进行下一轮匹配
+                    {
+                        continue;
+                    }
+                    points[0].X = tgx;
+                    points[0].Y = tgy;
+                    points[1].X = tgx + mtMainW;
+                    points[1].Y = tgy;
+                    points[2].X = tgx + mtMainW;
+                    points[2].Y = tgy + mtMainH;
+                    points[3].X = tgx;
+                    points[3].Y = tgy + mtMainH;
+                    return points;
+                }
+            }
+            return points;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 匹配第一个数字
+        /// </summary>
+        /// <param name="bmpTg"></param>
+        /// <param name="bmpMT"></param>
+        /// <param name="redress"></param>
+        /// <returns></returns>
+        public static bool MatchFirstNum(Bitmap bmpTg, Bitmap bmpMT, int redress)
+        {
+
+            int mtMainPxSum = 0;//模板主体像素数
+            int matchPxNum = 0;//目标图片与模板主体匹配成功的像素数
+
+            int tmpRGB = 0;
+            int mtxDiff = 0;//模板图片主体x起始坐标距离0的距离
+            int mtyDiff = 0;//模板图片主体y起始坐标距离0的距离
+            int mtMainW = 0;//模板图片主体宽
+            int mtMainH = 0;//模板图片主体高
+            List<int> mtxs = new List<int>();//模板图片x坐标集合
+            List<int> mtys = new List<int>();//模板图片y坐标集合
+
+            for (int x = 0; x < bmpMT.Width; x++)
+            {
+                for (int y = 0; y < bmpMT.Height; y++)
+                {
+                    if (bmpMT.GetPixel(x, y).B < 150)
+                    {
+                        mtxs.Add(x);
+                        mtys.Add(y);
+                    }
+                }
+            }
+
+            mtxDiff = mtxs.Min() - 0;
+            mtyDiff = mtys.Min() - 0;
+            mtMainW = mtxs.Max() - mtxs.Min() + 1;
+            mtMainH = mtys.Max() - mtys.Min() + 1;
+
+            for (int tgx = 0; tgx < (bmpTg.Width / 2) - mtMainW; tgx++)
+            {
+                for (int tgy = 0; tgy < bmpTg.Height - mtMainH; tgy++)
+                {
+                    mtMainPxSum = 0;
+                    matchPxNum = 0;
+                    for (int mtx = 0; mtx < mtMainW; mtx++)
+                    {
+                        for (int mty = 0; mty < mtMainH; mty++)
+                        {
+                            if (bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B > 100)
+                                continue;
+                            mtMainPxSum++;//统计模板主体像素数
+                            tmpRGB = bmpTg.GetPixel(tgx + mtx, tgy + mty).B - bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B;
+                            tmpRGB = tmpRGB > 0 ? tmpRGB : tmpRGB * (-1);
+                            if (tmpRGB < 150)//如果匹配
+                            {
+                                matchPxNum++;//统计匹配的像素数
+                            }
+                        }
+                    }
+                    //模板匹配结束一轮后
+                    int accuracy = (int)((matchPxNum * 1.0f / mtMainPxSum) * 100);//正确率
+                    if (accuracy + redress < 100)//如果正确率加上容错率没有大于百分百，说明没有匹配上，进行下一轮匹配
+                    {
+                        continue;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// 匹配运算结果数字
+        /// </summary>
+        /// <param name="bmpTg"></param>
+        /// <param name="bmpMT"></param>
+        /// <param name="redress"></param>
+        /// <returns></returns>
+        public static bool MatchResultNum(Bitmap bmpTg, Bitmap bmpMT, int redress)
+        {
+
+            int mtMainPxSum = 0;//模板主体像素数
+            int matchPxNum = 0;//目标图片与模板主体匹配成功的像素数
+
+            int tmpRGB = 0;
+            int mtxDiff = 0;//模板图片主体x起始坐标距离0的距离
+            int mtyDiff = 0;//模板图片主体y起始坐标距离0的距离
+            int mtMainW = 0;//模板图片主体宽
+            int mtMainH = 0;//模板图片主体高
+            List<int> mtxs = new List<int>();//模板图片x坐标集合
+            List<int> mtys = new List<int>();//模板图片y坐标集合
+
+            for (int x = 0; x < bmpMT.Width; x++)
+            {
+                for (int y = 0; y < bmpMT.Height; y++)
+                {
+                    if (bmpMT.GetPixel(x, y).B < 150)
+                    {
+                        mtxs.Add(x);
+                        mtys.Add(y);
+                    }
+                }
+            }
+
+            mtxDiff = mtxs.Min() - 0;
+            mtyDiff = mtys.Min() - 0;
+            mtMainW = mtxs.Max() - mtxs.Min() + 1;
+            mtMainH = mtys.Max() - mtys.Min() + 1;
+
+            for (int tgx = (bmpTg.Width / 2); tgx < (bmpTg.Width / 2) - mtMainW; tgx++)
+            {
+                for (int tgy = 0; tgy < bmpTg.Height - mtMainH; tgy++)
+                {
+                    mtMainPxSum = 0;
+                    matchPxNum = 0;
+                    for (int mtx = 0; mtx < mtMainW; mtx++)
+                    {
+                        for (int mty = 0; mty < mtMainH; mty++)
+                        {
+                            if (bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B > 100)
+                                continue;
+                            mtMainPxSum++;//统计模板主体像素数
+                            tmpRGB = bmpTg.GetPixel(tgx + mtx, tgy + mty).B - bmpMT.GetPixel(mtx + mtxDiff, mty + mtyDiff).B;
+                            tmpRGB = tmpRGB > 0 ? tmpRGB : tmpRGB * (-1);
+                            if (tmpRGB < 150)//如果匹配
+                            {
+                                matchPxNum++;//统计匹配的像素数
+                            }
+                        }
+                    }
+                    //模板匹配结束一轮后
+                    int accuracy = (int)((matchPxNum * 1.0f / mtMainPxSum) * 100);//正确率
+                    if (accuracy + redress < 100)//如果正确率加上容错率没有大于百分百，说明没有匹配上，进行下一轮匹配
+                    {
+                        continue;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 图片是否匹配
+        /// </summary>
+        /// <param name="bmpTg"></param>
+        /// <param name="bmpMT"></param>
+        /// <param name="redress"></param>
+        /// <returns></returns>
+        public static bool IsMatch(Bitmap bmpTg, Bitmap bmpMT, int redress)
+        {
+            if (GetTargetPoints(bmpTg, bmpMT, redress)[2].X > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
